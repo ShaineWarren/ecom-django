9b7864fb6faa93c6400b08fc704a6438bf4d50f2
ShaineWarren <mwarren86@gmail.com>
2022-06-22 19:14:26 -0700

    fix

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
